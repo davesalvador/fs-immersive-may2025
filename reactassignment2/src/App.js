aed91dfae03bedc1c03d41e773ba36ed44d962b8
davesalvador <101625754+davesalvador@users.noreply.github.com>
2025-07-31 08:42:06 +0800

    React Assignment 2

---
+++ reactassignment2/src/App.js
@@ -0,0 +1,12 @@
+import PersonalInfo from './component/PersonalInfo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactassignment2/src/component/PersonalInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PersonalInfo() {
+  return (
+    <div>
+        <h1>Dave</h1>
+        <h1>Salvador</h1>
+        <h1>Sinigang</h1>
+        <h1>Beach</h1>
+    </div>
+  )
+}
+
+export default PersonalInfo
